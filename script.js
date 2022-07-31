65b9d1948e26f580870d31f97c9a89cb9c905ecb
Enes Dogan <enesdoganlive@hotmail.com>
2022-08-01 00:24:26 +0300

    js homework 1 completed

--- script.js
+++ script.js
@@ -2,38 +2,66 @@ const name = prompt("Adınızı girin");
 
 document.querySelector(".name").innerHTML = name;
 
-const date = new Date();
+function showTime() {
+  var date = new Date();
 
-let hour = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
+  let dayNumber = date.getDay();
+  let dayName;
+  switch (dayNumber) {
+    case 0:
+      dayName = "Pazar";
+      break;
+    case 1:
+      dayName = "Pazartesi";
+      break;
+    case 2:
+      dayName = "Salı";
+      break;
+    case 3:
+      dayName = "Çarşamba";
+      break;
+    case 4:
+      dayName = "Perşembe";
+      break;
+    case 5:
+      dayName = "Cuma";
+      break;
+    case 6:
+      dayName = "Cumartesi";
+      break;
+    default:
+      dayName = "";
+  }
 
-let dayNumber = date.getDay();
-let dayName;
-switch (dayNumber) {
-  case 0:
-    dayName = "Pazartesi";
-    break;
-  case 1:
-    dayName = "Salı";
-    break;
-  case 2:
-    dayName = "Çarşamba";
-    break;
-  case 3:
-    dayName = "Perşembe";
-    break;
-  case 4:
-    dayName = "Cuma";
-    break;
-  case 5:
-    dayName = "Cumartesi";
-    break;
-  case 6:
-    dayName = "Pazar";
-    break;
-  default:
-    dayName = "";
+  document.getElementById("dayName").innerHTML = dayName;
+
+  var hour = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  if (hour == 0) {
+    hour == 12;
+  }
+
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  var time = hour + ":" + minutes + ":" + seconds;
+
+  document.querySelector(".time").textContent = time;
+  setTimeout(showTime, 1000);
 }
 
-alert(dayNumber);
+showTime();
+
